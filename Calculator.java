358c174ad580927aede204be13455fbf2625cd9e
Azaan Ahmed <azaanbaba007@gmail.com>
2025-01-25 23:14:05 +0530

    Changes

--- Calculator.java
+++ Calculator.java
@@ -3,8 +3,6 @@ import java.util.*;
 public class Calculator{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter What Operation You want to commit: \n1) Sum");
-        int op = sc.nextInt();
         System.out.print("Enter First Number : ");
         int num1 = sc.nextInt();
         System.out.print("Enter Second number : ");
